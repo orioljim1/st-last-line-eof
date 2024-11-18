584c7dba16f9d8db566d946cb6e8b425d1d509d3
orioljim1 <orioljay@gmail.com>
2024-11-18 22:12:51 +0100

    Added upper line logic too

--- st_last_line_down_eof.py
+++ st_last_line_down_eof.py
@@ -1,17 +1,27 @@
 import sublime
 import sublime_plugin
 
-class SmartDownCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+class SmartVerticalNavigationCommand(sublime_plugin.TextCommand):
+    def run(self, edit, direction="down"):
         cursor = self.view.sel()[0]
         cursor_row, _ = self.view.rowcol(cursor.begin())
+        
         last_row = self.view.rowcol(self.view.size())[0]
-        if cursor_row == last_row:
+        
+        if direction == "down" and cursor_row == last_row:
             self.view.run_command("move_to", {"to": "eof"})
+        elif direction == "up" and cursor_row == 0:
+            self.view.run_command("move_to", {"to": "bof"})
         else:
-            self.view.run_command("move", {"by": "lines", "forward": True})
+            self.view.run_command("move", {
+                "by": "lines",
+                "forward": direction == "down"
+            })
 
-class SmartDownListener(sublime_plugin.EventListener):
+class SmartNavigationListener(sublime_plugin.EventListener):
     def on_text_command(self, view, command_name, args):
-        if command_name == "move" and args == {"by": "lines", "forward": True}:
-            return ("smart_down", {})
+        if command_name == "move":
+            if args == {"by": "lines", "forward": True}:
+                return ("smart_vertical_navigation", {"direction": "down"})
+            elif args == {"by": "lines", "forward": False}:
+                return ("smart_vertical_navigation", {"direction": "up"})
